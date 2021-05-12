971d346c29521beed381e820765848d1c42c1fa9
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2021-05-11 22:06:32 -0500

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,15 +1,25 @@
-def unlock():
-    global a
-    if password == "abcd":
-        a = 1
-    print("Evaluating password")
-    while a < 4:
-        print(str(a) + "...")
-        a += 1
-    print("Access Granted")
+prompt = input("Would you like to access this program?: ")
+while prompt == "Yes":
+    text = input("Would you like to access your account: ")
+    if text == "Yes":
+        username = input("Enter your username: ")
+        if username == "Colin":
+            password = input("Welcome Colin what is your password?: ")
+            if password == "abcd":
+                a = 1
+                print("Evaluating password")
+                while a < 4:
+                    print(str(a) + "...")
+                    a += 1
+                print("Access Granted")
+            else:
+                print("Try again")
+        else:
+            print("Enter a valid username")
+        break
+if prompt == "No":
+    print("Thank You and come again")
 
 
-username = input("What is the username?: ")
-if username == "Colin":
-    password = input("Welcome Colin what is your password?: ")
-    unlock()
+
+
